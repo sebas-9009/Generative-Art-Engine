6d317aefc5af185cce5cfa3bc65257247c4cab3b
Sebastian Meneses Rios <a2017114961@estudiantes.upsa.edu.bo>
2021-10-04 04:42:57 -0400

    Generative Art Engine for Llamas

---
+++ src/config.js
@@ -0,0 +1,67 @@
+"use strict";
+
+const path = require("path");
+const isLocal = typeof process.pkg === "undefined";
+const basePath = isLocal ? process.cwd() : path.dirname(process.execPath);
+const { MODE } = require(path.join(basePath, "src/blendMode.js"));
+const description =
+  "The Llamas Society INC.";
+const baseUri = "ipfs://NewUriToReplace";
+
+const layerConfigurations = [
+  {
+    growEditionSizeTo: 100,
+    layersOrder: [
+      { name: "Background" },
+      { name: "CIRCLE" },
+      { name: "SKIN" },
+      { name: "FUR" },
+      { name: "MOUTH" },
+      { name: "EYES" },
+      { name: "DETAILEYE" },
+      { name: "OUTFITS" },
+      { name: "EARS" },
+    ],
+  },
+];
+
+const shuffleLayerConfigurations = false;
+
+const debugLogs = false;
+
+const format = {
+  width: 512,
+  height: 512,
+};
+
+const background = {
+  generate: true,
+  brightness: "80%",
+};
+
+const extraMetadata = {};
+
+const rarityDelimiter = "#";
+
+const uniqueDnaTorrance = 10000;
+
+const preview = {
+  thumbPerRow: 5,
+  thumbWidth: 50,
+  imageRatio: format.width / format.height,
+  imageName: "preview.png",
+};
+
+module.exports = {
+  format,
+  baseUri,
+  description,
+  background,
+  uniqueDnaTorrance,
+  layerConfigurations,
+  rarityDelimiter,
+  preview,
+  shuffleLayerConfigurations,
+  debugLogs,
+  extraMetadata,
+};
